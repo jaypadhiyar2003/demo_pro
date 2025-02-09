757a2805b5572974ea42a0f3e1fd21185837368b
jaypadhiyar2003 <padhiyarjay77@gmail.com>
2025-02-09 21:55:27 +0530

    logo url changed

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -12,7 +12,7 @@ global $serv;
   <span class="block sm:inline">Try to login in your account</span>
 </div>' : '' ?>
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
     <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Login to your account</h2>
   </div>
 
